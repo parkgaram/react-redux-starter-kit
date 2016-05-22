bb676765c32f9aa3c49a948b827cff92e9462f53
garam <netgarng88@gmail.com>
2016-05-22 21:54:44 +0900

    chang component style

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,24 +1,34 @@
-import React from 'react'
+import React,{ Component } from 'react'
 import classes from './Counter.scss'
 
-export const Counter = (props) => (
-  <div>
-    <h2 className={classes.counterContainer}>
-      Counter:
-      {' '}
-      <span className={classes['counter--green']}>
-        {props.counter}
-      </span>
-    </h2>
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
-  </div>
-)
+class Counter extends Component{
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { counter, increment, doubleAsync } = this.props;
+    return (
+      <div>
+        <h2 className={classes.counterContainer}>
+          Counter:
+          {' '}
+          <span className={classes['counter--green']}>
+            {counter}
+          </span>
+        </h2>
+        <button className='btn btn-default' onClick={increment}>
+          Increment
+        </button>
+        {' '}
+        <button className='btn btn-default' onClick={doubleAsync}>
+          Double (Async)
+        </button>
+      </div>
+    )
+  }
+}
 
 Counter.propTypes = {
   counter: React.PropTypes.number.isRequired,
